4561ab0beeef8e9fcffe896147e479cff51e7170
Somnath Nandi <154261909+CodeOfSomnathNandi@users.noreply.github.com>
2024-02-03 21:02:59 +0530

    adding move functions

--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,9 @@
+import java.io.IOException;
+
 import com.kernel.*;
 
 public class Main {
-    public static void main(String[] args) {
-        SimpleFunctions.SetLastError(2);
-       System.out.println(SimpleFunctions.GetLastError());
-       System.out.println();
+    public static void main(String[] args) throws IOException {
+       FileOperations.CopyFile("C:\\Users\\somna\\OneDrive\\Documents\\vs_code\\win32-java\\README.MD", "ehdo.txt");
     }
 }
--- src/com/kernel/FileOperations.java
+++ src/com/kernel/FileOperations.java
@@ -5,12 +5,12 @@ import java.io.IOException;
 
 public class FileOperations {
     
-    public  boolean CopyFile(String existingFilePath, String newFilePath) throws IOException {
+    public static boolean CopyFile(String existingFilePath, String newFilePath) throws IOException {
         
         // clearing previous error value
         SimpleFunctions.GetLastError();
 
-        if (existingFilePath == null && newFilePath == null) {
+        if (existingFilePath == null || newFilePath == null) {
             throw new IOException("File path is null");
         }
 
@@ -20,15 +20,42 @@ public class FileOperations {
         }
         var error_no = SimpleFunctions.GetLastError();
         if (error_no == WinErrorCodes.ERROR_FILE_NOT_FOUND.getValue()) {
-            throw new FileNotFoundException(String.format("{} Not Found.", existingFilePath));
+            throw new FileNotFoundException(String.format("%s Not Found.", existingFilePath));
         }
         
         if (WinErrorCodes.ERROR_ACCESS_DENIED.getValue() == error_no) {
-            throw new IOException(String.format("{} Access Denied.", existingFilePath));
+            throw new IOException(String.format("%s Access Denied.", existingFilePath));
         }
         
 
         return false;
+    }
+
+
+    public static void MoveFile(String existingFileName, String newFileName) throws IOException {
+        // resetting error no.
+        SimpleFunctions.GetLastError();
+
+        if (existingFileName == null || newFileName == null) {
+            throw new IOException("File path is null");
+        }
+
+        boolean isSuccess =  SimpleFunctions.MoveFile(existingFileName, newFileName);
+
+        if (isSuccess) {
+            return;
+        }
+
+        long error = SimpleFunctions.GetLastError();
+
+        if (error == WinErrorCodes.ERROR_PATH_NOT_FOUND.getValue()) {
+            throw new IOException("Path Not Found.");
+        }
+
+        if (error != 0) {
+            throw new IOException(String.format("Native function throw error: %d", error));
+        }
+
     }
 
     // @Override
