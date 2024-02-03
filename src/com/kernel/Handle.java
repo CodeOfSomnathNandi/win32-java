c28a31075b472cb85e88bdc4aa2956d6d1766ff5
Somnath Nandi <154261909+CodeOfSomnathNandi@users.noreply.github.com>
2024-02-04 00:31:06 +0530

    This is a simple handle supported

--- src/com/kernel/Handle.java
+++ src/com/kernel/Handle.java
@@ -1,6 +1,14 @@
 package com.kernel;
 
 public class Handle {
+    private long handleValue = -1;
 
+    public Handle(long value) {
+        this.handleValue = value;
+    }
     
+    public long getHandleValue() {
+        return this.handleValue;
+    }
+
 }
