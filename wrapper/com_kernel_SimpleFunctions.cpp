d37d3f4414332169718b89c75a2853bdea8445c4
Somnath Nandi <154261909+CodeOfSomnathNandi@users.noreply.github.com>
2024-02-05 21:43:25 +0530

    new functions implementated

--- wrapper/com_kernel_SimpleFunctions.cpp
+++ wrapper/com_kernel_SimpleFunctions.cpp
@@ -61,3 +61,55 @@ JNIEXPORT jboolean JNICALL Java_com_kernel_SimpleFunctions_CreateDirectory(JNIEn
         return JNI_FALSE;
     }
 }
+
+JNIEXPORT jboolean JNICALL Java_com_kernel_SimpleFunctions_DeleteFile(JNIEnv *env, jclass, jstring fileName)
+{
+    char *cfileName = jstring_to_cstr(env, fileName);
+    int isSuccess = DeleteFileA(cfileName);
+    if (isSuccess == TRUE)
+    {
+        return JNI_TRUE;
+    }
+    else
+    {
+        return JNI_FALSE;
+    }
+}
+
+JNIEXPORT jboolean JNICALL Java_com_kernel_SimpleFunctions_MoveFile(JNIEnv *env, jclass, jstring existingFileName, jstring newFileName)
+{
+    char *lpExistingFileName = jstring_to_cstr(env, existingFileName);
+    char *lpNewFileName = jstring_to_cstr(env, newFileName);
+
+    int isSuccess = MoveFileA(lpExistingFileName, lpNewFileName);
+
+    if (isSuccess == TRUE)
+    {
+        return JNI_TRUE;
+    }
+    else
+    {
+        return JNI_FALSE;
+    }
+}
+
+JNIEXPORT jstring JNICALL Java_com_kernel_SimpleFunctions_GetCurrentDirectory(JNIEnv * env, jclass)
+{
+    const int size = 2056;
+    LPSTR buffer = new char[size];
+    DWORD length = GetCurrentDirectoryA(size, buffer);
+    LPSTR newBuff = new char[length];
+
+    for (DWORD i = 0; i < length; i++)
+    {
+        newBuff[i] = buffer[i];
+    }
+    
+
+    jstring buff = env->NewStringUTF(newBuff);
+
+    delete newBuff;
+    delete buffer;
+
+    return buff;
+}
