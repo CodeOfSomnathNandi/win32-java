64ae236c7522cfe2048d7632cd98c5a56cf3f7c9
Somnath Nandi <154261909+CodeOfSomnathNandi@users.noreply.github.com>
2024-02-06 00:26:39 +0530

    implementing functions

--- wrapper/com_kernel_SimpleFunctions.cpp
+++ wrapper/com_kernel_SimpleFunctions.cpp
@@ -36,6 +36,9 @@ JNIEXPORT jboolean JNICALL Java_com_kernel_SimpleFunctions_CopyFile(JNIEnv *env,
     char *existingFileName = jstring_to_cstr(env, lpExistingFileName);
     int success = CopyFileA(existingFileName, newFileName, (bFailIfExists == JNI_TRUE));
 
+    delete newFileName;
+    delete existingFileName;
+
     if (success == TRUE)
     {
         return JNI_TRUE;
@@ -49,8 +52,9 @@ JNIEXPORT jboolean JNICALL Java_com_kernel_SimpleFunctions_CopyFile(JNIEnv *env,
 JNIEXPORT jboolean JNICALL Java_com_kernel_SimpleFunctions_CreateDirectory(JNIEnv *env, jclass, jstring lpPathName)
 {
     char *pathName = jstring_to_cstr(env, lpPathName);
-
     int isSuccess = CreateDirectoryA(pathName, NULL);
+
+    delete pathName;
 
     if (isSuccess == TRUE)
     {
@@ -66,6 +70,9 @@ JNIEXPORT jboolean JNICALL Java_com_kernel_SimpleFunctions_DeleteFile(JNIEnv *en
 {
     char *cfileName = jstring_to_cstr(env, fileName);
     int isSuccess = DeleteFileA(cfileName);
+
+    delete cfileName;
+
     if (isSuccess == TRUE)
     {
         return JNI_TRUE;
@@ -80,8 +87,10 @@ JNIEXPORT jboolean JNICALL Java_com_kernel_SimpleFunctions_MoveFile(JNIEnv *env,
 {
     char *lpExistingFileName = jstring_to_cstr(env, existingFileName);
     char *lpNewFileName = jstring_to_cstr(env, newFileName);
-
     int isSuccess = MoveFileA(lpExistingFileName, lpNewFileName);
+
+    delete lpExistingFileName;
+    delete lpNewFileName;
 
     if (isSuccess == TRUE)
     {
@@ -93,7 +102,7 @@ JNIEXPORT jboolean JNICALL Java_com_kernel_SimpleFunctions_MoveFile(JNIEnv *env,
     }
 }
 
-JNIEXPORT jstring JNICALL Java_com_kernel_SimpleFunctions_GetCurrentDirectory(JNIEnv * env, jclass)
+JNIEXPORT jstring JNICALL Java_com_kernel_SimpleFunctions_GetCurrentDirectory(JNIEnv *env, jclass)
 {
     const int size = 2056;
     LPSTR buffer = new char[size];
@@ -104,7 +113,6 @@ JNIEXPORT jstring JNICALL Java_com_kernel_SimpleFunctions_GetCurrentDirectory(JN
     {
         newBuff[i] = buffer[i];
     }
-    
 
     jstring buff = env->NewStringUTF(newBuff);
 
@@ -112,4 +120,100 @@ JNIEXPORT jstring JNICALL Java_com_kernel_SimpleFunctions_GetCurrentDirectory(JN
     delete buffer;
 
     return buff;
+}
+
+JNIEXPORT jboolean JNICALL Java_com_kernel_SimpleFunctions_CreateSymbolicLinkA(JNIEnv *env, jclass, jstring symlinkFileName, jstring targetFileName, jlong dwFlags)
+{
+    char *lpSymlinkFileName = jstring_to_cstr(env, symlinkFileName);
+    char *lpTargetFileName = jstring_to_cstr(env, targetFileName);
+    int isSuccess = CreateSymbolicLinkA(lpSymlinkFileName, lpTargetFileName, (DWORD)dwFlags);
+
+    delete lpSymlinkFileName;
+    delete lpTargetFileName;
+
+    if (isSuccess == TRUE)
+    {
+        return JNI_TRUE;
+    }
+    else
+    {
+        return JNI_FALSE;
+    }
+}
+
+JNIEXPORT jboolean JNICALL Java_com_kernel_SimpleFunctions_DecryptFileA(JNIEnv *env, jclass, jstring fileName, jlong dwReserved)
+{
+    char *lpFileName = jstring_to_cstr(env, fileName);
+    int isSuccess = DecryptFileA(lpFileName, (DWORD)dwReserved);
+
+    delete lpFileName;
+
+    if (isSuccess == TRUE)
+    {
+        return JNI_TRUE;
+    }
+    else
+    {
+        return JNI_FALSE;
+    }
+}
+
+JNIEXPORT jboolean JNICALL Java_com_kernel_SimpleFunctions_EncryptFileA(JNIEnv *env, jclass, jstring fileName)
+{
+    char *lpFileName = jstring_to_cstr(env, fileName);
+    int isSuccess = EncryptFileA(lpFileName);
+
+    delete lpFileName;
+
+    if (isSuccess == TRUE)
+    {
+        return JNI_TRUE;
+    }
+    else
+    {
+        return JNI_FALSE;
+    }
+}
+
+JNIEXPORT jboolean JNICALL Java_com_kernel_SimpleFunctions_FileEncryptionStatusA(JNIEnv *env, jclass, jstring fileName, jlong status)
+{
+    char *lpFileName = jstring_to_cstr(env, fileName);
+    DWORD lpStatus;
+    int isSuccess = FileEncryptionStatusA(lpFileName, &lpStatus);
+    status = (jlong)lpStatus;
+
+    delete lpFileName;
+
+    if (isSuccess == TRUE)
+    {
+        return JNI_TRUE;
+    }
+    else
+    {
+        return JNI_FALSE;
+    }
+}
+
+JNIEXPORT jlong JNICALL Java_com_kernel_SimpleFunctions_GetFileSize(JNIEnv *env, jclass, jstring fileName)
+{
+    char *lpFileName = jstring_to_cstr(env, fileName);
+
+    HANDLE fileHandle = CreateFileA(lpFileName, GENERIC_ALL, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    DWORD fileSize;
+    GetFileSize(fileHandle, &fileSize);
+
+    delete lpFileName;
+
+    return (jlong)fileSize;
+}
+
+JNIEXPORT jlong JNICALL Java_com_kernel_SimpleFunctions_GetFileType(JNIEnv *env, jclass, jstring fileName)
+{
+    char *lpFileName = jstring_to_cstr(env, fileName);
+    HANDLE fileHandle = CreateFileA(lpFileName, GENERIC_ALL, FILE_SHARE_READ, NULL, OPEN_EXISTING, FILE_ATTRIBUTE_NORMAL, NULL);
+    DWORD fileType = GetFileType(fileHandle);
+    
+    delete lpFileName;
+    
+    return (jlong)fileType;
 }
