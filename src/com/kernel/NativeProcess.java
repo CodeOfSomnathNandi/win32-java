587baa320f6dd82f6d6cebdefb118183628c838a
Somnath Nandi <154261909+CodeOfSomnathNandi@users.noreply.github.com>
2024-02-06 13:55:42 +0530

    adding simple updates in jni

--- src/com/kernel/NativeProcess.java
+++ src/com/kernel/NativeProcess.java
@@ -22,6 +22,7 @@ public class NativeProcess {
         private static native long OpenProcess(long dwDesiredAccess, boolean bInheritHandle, long dwProcessId);
 
         private static native boolean TerminateProcess(long hProcess, int uExitCode);
+        private static native long GetCurrentProcessId();
 
         private Handle threadHandle;
 
@@ -96,6 +97,10 @@ public class NativeProcess {
         }
 
         public long getPID() {
+                return GetCurrentProcessId();
+        }
+
+        public long getCurrentProccessorNumber() {
                 return GetCurrentProcessorNumber();
         }
 
---
+++ wrapper/com_kernel_NativeProcess.cpp
@@ -0,0 +1,77 @@
+#include "com_kernel_NativeProcess.h"
+#include "utility.h"
+#include <windows.h>
+
+JNIEXPORT jlong JNICALL Java_com_kernel_NativeProcess_CreateProcessA(JNIEnv *env, jclass, jstring applicationName, jstring commandLine, jboolean bInheritHandles, jlong dwCreationFlags, jlong lpEnvironment, jstring currentDirectory)
+{
+    char *lpApplicationName = jstring_to_cstr(env, applicationName);
+    char *lpCommandLine = jstring_to_cstr(env, commandLine);
+    char *lpCurrentDirectory = jstring_to_cstr(env, currentDirectory);
+    PROCESS_INFORMATION pi;
+    CreateProcessA(lpApplicationName, lpCommandLine, NULL, NULL, bInheritHandles, dwCreationFlags, NULL, lpCurrentDirectory, NULL, &pi);
+
+    return (jlong)pi.hProcess;
+}
+
+JNIEXPORT void JNICALL Java_com_kernel_NativeProcess_ExitProcess(JNIEnv *, jclass, jint exitCode)
+{
+    ExitProcess(exitCode);
+}
+
+JNIEXPORT jstring JNICALL Java_com_kernel_NativeProcess_GetCommandLineA(JNIEnv *env, jclass)
+{
+    LPSTR lpCmdLine = GetCommandLineA();
+    jstring cmdLine = env->NewStringUTF(lpCmdLine);
+    return cmdLine;
+}
+
+JNIEXPORT jlong JNICALL Java_com_kernel_NativeProcess_GetCurrentProcess(JNIEnv *, jclass)
+{
+    HANDLE currentHandle = GetCurrentProcess();
+    return (jlong)currentHandle;
+}
+
+JNIEXPORT jlong JNICALL Java_com_kernel_NativeProcess_GetCurrentProcessorNumber(JNIEnv *, jclass)
+{
+    return GetCurrentProcessorNumber();
+}
+
+JNIEXPORT jstring JNICALL Java_com_kernel_NativeProcess_GetEnvironmentVariable(JNIEnv *env, jclass, jstring envName)
+{
+    char *lpEnvName = jstring_to_cstr(env, envName);
+    const int size = 2056;
+    char *lpValue = new char[size];
+    long newSize = GetEnvironmentVariableA(lpEnvName, lpValue, size);
+    char *finalValue = new char[newSize + 1];
+    for (int i = 0; i < newSize; i++)
+    {
+        finalValue[i] = lpValue[i];
+    }
+
+    finalValue[newSize] = '\0';
+
+    jstring value = env->NewStringUTF(finalValue);
+
+    return value;
+}
+
+JNIEXPORT jintArray JNICALL Java_com_kernel_NativeProcess_GetProcessTimes(JNIEnv * env, jclass, jlong hProcess)
+{
+    FILETIME creationTime;
+    FILETIME exitTime;
+    FILETIME kernelTime;
+    FILETIME userTime;
+    GetProcessTimes((HANDLE)hProcess, &creationTime, &exitTime, &kernelTime, &userTime);
+
+    jintArray jarr = env->NewIntArray(4);
+
+    jint * arr =  env->GetIntArrayElements(jarr, NULL);
+
+    arr[0] = creationTime.dwHighDateTime;
+    arr[1] = exitTime.dwHighDateTime;
+    arr[2] = kernelTime.dwHighDateTime;
+    arr[3] = userTime.dwHighDateTime;
+    
+    return jarr;
+
+}
